14fc89a6786a96a157a86491c713cf6377b6dbc4
Julio <julio.barravalenzuela@gmail.com>
2022-11-21 01:19:31 -0300

    Avance peuqe

---
+++ obtenerDatos.py
@@ -0,0 +1,7 @@
+import wikipedia as wiki
+
+#busqueda = ['Chile', 'Argentina', 'Bolivia', 'Perú', 'Mexico', 'Brazil', 'Colombia', 'Ecuador', 'Venezuela', 'Uruguay']
+
+a = wiki.page("Uruguay")
+
+print(a.content)
